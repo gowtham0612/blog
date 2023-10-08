b95c1abbc162a05efa41c2875a34b58fd560e435
gowthamEntrans <gowtham@entrans.io>
2023-10-08 23:36:37 +0530

    first commit

---
+++ src/Pages/page-1/Firstpage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Page from "../../Components/Page-1/Firstcomponent";
+
+const Firstpage = () => {
+  return (
+    <>
+      <Page />
+    </>
+  );
+};
+
+export default Firstpage;
